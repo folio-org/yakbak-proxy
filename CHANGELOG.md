9fca025260b03d5cdf1f1a949d9cf05034f8a1eb
Mike Taylor <mike@indexdata.com>
2020-07-15 17:02:27 +0100

    Release 1.2, adds -q option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Change history for yakbak-proxy
 
+## [1.2.0](https://github.com/folio-org/yakbak-proxy/tree/v1.2.0) (2020-07-15)
+[Full Changelog](https://github.com/folio-org/yakbak-proxy/compare/v1.1.0...v1.2.0)
+
+* Documentation in the [`README.md`](README.md).
+* Add a [LICENSE](LICENSE) file.
+
 ## [1.1.0](https://github.com/folio-org/yakbak-proxy/tree/v1.1.0) (2020-07-03)
 [Full Changelog](https://github.com/folio-org/yakbak-proxy/compare/v1.0.0...v1.1.0)
 
--- yakbak-proxy.js
+++ yakbak-proxy.js
@@ -12,6 +12,7 @@ const optionDefinitions = [
   { name: 'verbose', alias: 'v', type: Boolean, defaultValue: false },
   { name: 'norecord', alias: 'n', type: Boolean, defaultValue: false },
   { name: 'ignoreheaders', alias: 'i', type: Boolean, defaultValue: false },
+  { name: 'sequence', alias: 'q', type: Boolean, defaultValue: false },
   { name: 'port', alias: 'p', type: Number, defaultValue: 3002 },
   { name: 'tapes', alias: 't', type: String, defaultValue: 'tapes' },
   { name: 'server', alias: 's', type: String, defaultOption: true },
@@ -27,6 +28,7 @@ Usage: ${process.argv[1]} [options] <serverUrl>
         -v|--verbose            Log parameters before starting server
         -n|--norecord           Fail requests that have no tape [default: false]
         -i|--ignoreheaders      Exclude headers from hash function [default: false]
+        -q|--sequence           Include sequence numbers in each request [default: false]
         -p|--port <num>         Listen on port <num> [default: 3002]
         -t|--tapes <dir>        Write tapes to <dir> [default: tapes]`
   );
@@ -42,9 +44,17 @@ if (options.verbose) {
   console.log(`listening on port ${options.port}, proxying to ${options.server}`);
 }
 
+let seq = 1;
 http.createServer(yakbak(options.server, {
   // Yakbak can't find its own tapes if this is not an absolute path
   dirname: path.resolve(options.tapes),
   noRecord: options.norecord,
-  hash: !options.ignoreheaders ? undefined : (req, body) => hash.sync({ ...req, headers: {} }, body),
+  hash: (req, body) => {
+    console.log('seq =', seq);
+    seq++;
+    const extras = {};
+    if (options.ignoreheaders) extras.headers = {};
+    if (options.sequence) extras.headers['X-sequence'] = seq;
+    return hash.sync({ ...req, ...extras }, body);
+  },
 })).listen(options.port);
