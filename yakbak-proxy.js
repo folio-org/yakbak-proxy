66a66b7e7054926e98f0a9526b7a099a973687d4
Mike Taylor <mike@indexdata.com>
2020-07-21 01:46:10 +0100

    Smarter sequence-numbering algorithm

--- yakbak-proxy.js
+++ yakbak-proxy.js
@@ -16,7 +16,6 @@ const optionDefinitions = [
   { name: 'norecord', alias: 'n', type: Boolean, defaultValue: false, description: 'Fail requests that have no tape' },
   { name: 'ignoreheaders', alias: 'i', type: Boolean, defaultValue: false, description: 'Exclude headers from hash function' },
   { name: 'exciseid', alias: 'x', type: Boolean, defaultValue: false, description: 'Excise "id" fields from POST requests' },
-  { name: 'sequence', alias: 'q', type: Boolean, defaultValue: false, description: 'Include sequence numbers in each request' },
   { name: 'port', alias: 'p', type: Number, defaultValue: 3002, description: 'Listen on port <num> [default: 3002]' },
   { name: 'tapes', alias: 't', type: String, defaultValue: 'tapes', description: 'Write tapes to <dir> [default: tapes]' },
   { name: 'server', alias: 's', type: String, defaultOption: true, description: 'Specify server to proxy for' },
@@ -45,15 +44,17 @@ if (!options.norecord && !options.server) {
 l.log('startup', `listening on port ${options.port},`,
   options.norecord ? 'not proxying' : `proxying to ${options.server}`);
 
-const counters = {};
+const counters = {}; // maps digests to count of unique responses
+let seenSinceWrite = {}; // maps digests to true if seen since last write
+
 http.createServer(yakbak(options.server, {
   // Yakbak can't find its own tapes if this is not an absolute path
   dirname: path.resolve(options.tapes),
   noRecord: options.norecord,
   hash: (req, originalBody) => {
-    let body = originalBody;
     const extras = {};
     if (options.ignoreheaders) extras.headers = {};
+    let body = originalBody;
     if (req.method === 'POST' && options.exciseid) {
       const s = body.toString();
       const j = JSON.parse(s);
@@ -63,14 +64,20 @@ http.createServer(yakbak(options.server, {
     }
 
     const digest = hash.sync({ ...req, ...extras }, body);
-    if (!options.sequence) {
-      l.log('request', digest.substring(0, 8), req.method, req.url);
-      return digest;
+
+    if ((req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE')
+        && !(req.url.match(/\/login[/?]/))) {
+      // Special case: no need to invalidate cache for a login
+      seenSinceWrite = {};
     }
 
-    const counter = counters[digest] || 0;
+    let counter = counters[digest] || 0;
+    if (!seenSinceWrite[digest]) {
+      seenSinceWrite[digest] = true;
+      counter += 1;
+      counters[digest] = counter;
+    }
     l.log('request', ' '.repeat(counter), counter, digest.substring(0, 8), req.method, req.url);
-    counters[digest] = counter + 1;
     return `${digest}-${counter}`;
   },
 })).listen(options.port);
